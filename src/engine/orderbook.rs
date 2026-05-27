5e3ae401b02052c2a5856c7db8d6a0710a8074f1
JunYoung Choi <juny0955@gmail.com>
2026-05-27 15:51:55 +0900

    refactor: 오더북 Queue orderId 기반 변경

--- src/engine/orderbook.rs
+++ src/engine/orderbook.rs
@@ -1,16 +1,19 @@
 use std::{
     cmp::Reverse,
-    collections::{BTreeMap, VecDeque},
+    collections::{BTreeMap, HashMap, VecDeque},
 };
 
 use rust_decimal::Decimal;
+use uuid::Uuid;
 
-use crate::domain::order::Order;
+use crate::domain::order::{Order, Side};
 
 #[derive(Debug, Default)]
 pub struct OrderBook {
     // 매수 호가 Price 내림차순
-    pub bids: BTreeMap<Reverse<Decimal>, VecDeque<Order>>,
+    bids: BTreeMap<Reverse<Decimal>, VecDeque<Uuid>>,
     // 매도 호가 Price 오름차순
-    pub asks: BTreeMap<Decimal, VecDeque<Order>>,
+    asks: BTreeMap<Decimal, VecDeque<Uuid>>,
+    // 실제 주문 데이터
+    index: HashMap<Uuid, Order>,
 }
