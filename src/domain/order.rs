82f06553a0ef462b0ebbcb990ae89b5d0ce114d0
JunYoung Choi <juny0955@gmail.com>
2026-05-28 10:35:18 +0900

    feat: OrderStatus 및 fill() 정합성 검사 추가

--- src/domain/order.rs
+++ src/domain/order.rs
@@ -20,6 +20,16 @@ pub enum TimeInForce {
     FOK,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum OrderStatus {
+    New,
+    PartiallyFilled,
+    Filled,
+    Canceled,
+    Rejected,
+    Expired,
+}
+
 #[derive(Debug, Clone)]
 pub struct Order {
     pub order_id: Uuid,
@@ -30,15 +40,30 @@ pub struct Order {
     pub price: Decimal,
     pub quantity: Decimal,
     pub remaining_quantity: Decimal,
+    pub status: OrderStatus,
     pub created_at: u64,
 }
 
 impl Order {
     pub fn fill(&mut self, fill_qty: Decimal) {
-        self.remaining_quantity -= fill_qty
+        assert!(fill_qty > Decimal::ZERO);
+        assert!(fill_qty <= self.remaining_quantity);
+        assert!(!self.is_complated());
+
+        self.remaining_quantity -= fill_qty;
+
+        self.status = if self.remaining_quantity.is_zero() {
+            OrderStatus::Filled
+        } else {
+            OrderStatus::PartiallyFilled
+        };
     }
 
     pub fn is_filled(&self) -> bool {
-        self.remaining_quantity.is_zero()
+        self.status == OrderStatus::Filled
+    }
+
+    fn is_complated(&self) -> bool {
+        !matches!(self.status, OrderStatus::PartiallyFilled | OrderStatus::New)
     }
 }
