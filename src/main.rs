5c27d120caef995c8bde97acf0b5f2ec0337fcc0
JunYoung Choi <juny0955@gmail.com>
2026-06-02 14:12:47 +0900

    feat: main.rs 연결

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
-use matching_engine::init::init;
+use matching_engine::{
+    engine::{
+        dispatcher::EngineDispatcher, result::EngineResult, result_handler::EngineResultHandler,
+    },
+    init::init,
+};
 
 fn main() {
     init();
+
+    let (result_tx, result_rx) = crossbeam::channel::bounded::<EngineResult>(1024);
+
+    let symbols = vec!["BTCUSDT".to_string()];
+    let _engine_dispatcher = EngineDispatcher::new(symbols, result_tx);
+
+    std::thread::spawn(move || {
+        EngineResultHandler::new(result_rx).run();
+    });
+
+    std::thread::park();
 }
